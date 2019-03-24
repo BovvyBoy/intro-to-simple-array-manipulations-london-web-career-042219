6dbdeda94807085464a4ed67aab07bffd7cb7c99
Daniel Bovington <d.bovington@gmail.com>
2019-03-24 13:35:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,9 +30,9 @@ end
 def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
-#def using_delete(instructors, "Steven")
-  #instructors.delete("Steven")
-#end
+def using_delete(instructors, @Steven)
+  instructors.delete("Steven")
+end
 #def using_delete_at(famous_robots, 2)
   #famous_robots.delete(2)
 #end
